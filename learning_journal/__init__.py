d9b174f891b7ad7fbbd3cfdf0d36e6a25de5f6ec
Salim Hamed <salimhamed@gmail.com>
2015-01-06 21:19:15 -0800

    Initialed Database.

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -1,14 +1,10 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
-
-from .models import (
-    DBSession,
-    Base,
-    )
-
+from .models import DBSession, Base
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
+    """
+    This function returns a Pyramid WSGI application.
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
--- learning_journal/models.py
+++ learning_journal/models.py
@@ -1,17 +1,6 @@
-from sqlalchemy import (
-    Column,
-    Index,
-    Integer,
-    Text,
-    )
-
+from sqlalchemy import Column, Index, Integer, Text
 from sqlalchemy.ext.declarative import declarative_base
-
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
-    )
-
+from sqlalchemy.orm import scoped_session, sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
