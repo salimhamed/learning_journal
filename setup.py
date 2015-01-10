f1a8bf5866be3be953b6ad6cd94a7fd532c62760
Salim Hamed <salimhamed@gmail.com>
2015-01-10 14:26:30 -0800

    Modified .gitignore file to exlude .egg-info and .swp files.  Also changed formatting in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-import os
-
 from setuptools import setup, find_packages
+import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.txt')) as f:
@@ -17,31 +16,33 @@ requires = [
     'transaction',
     'zope.sqlalchemy',
     'waitress',
-    ]
+]
 
-setup(name='learning_journal',
-      version='0.0',
-      description='learning_journal',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
+setup(
+    name='learning_journal',
+    version='0.1',
+    description='learning_journal',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='learning_journal',
-      install_requires=requires,
-      entry_points="""\
-      [paste.app_factory]
-      main = learning_journal:main
-      [console_scripts]
-      initialize_learning_journal_db = learning_journal.scripts.initializedb:main
-      """,
-      )
+    ],
+    author='Salim Hamed',
+    author_email='salimhamed@gmail.com',
+    url='',
+    keywords='web wsgi bfg pylons pyramid',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='learning_journal',
+    install_requires=requires,
+    # entry points are ways we can run our code once installed
+    entry_points="""\
+    [paste.app_factory]
+    main = learning_journal:main
+    [console_scripts]
+    initialize_learning_journal_db = learning_journal.scripts.initializedb:main
+    """,
+)
