1355d06885ecd7ea22a80f13708552d14471d311
Salim Hamed <salimhamed@gmail.com>
2015-01-10 14:51:19 -0800

    formatting changes to initalizedb.py file

--- learning_journal/scripts/initializedb.py
+++ learning_journal/scripts/initializedb.py
@@ -1,21 +1,10 @@
+from ..models import DBSession, MyModel, Base
+from pyramid.paster import get_appsettings, setup_logging
+from pyramid.scripts.common import parse_vars
+from sqlalchemy import engine_from_config
 import os
 import sys
 import transaction
-
-from sqlalchemy import engine_from_config
-
-from pyramid.paster import (
-    get_appsettings,
-    setup_logging,
-    )
-
-from pyramid.scripts.common import parse_vars
-
-from ..models import (
-    DBSession,
-    MyModel,
-    Base,
-    )
 
 
 def usage(argv):
